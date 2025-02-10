987047ee3f64b43e8b04b9a87643edce5479f12e
Eder Vieira de Oliveira Ribeiro <eder.edy@gmail.com>
2025-02-10 13:09:29 -0400

    Criado crud api, validação

--- Blog/Controllers/CategoryController.cs
+++ Blog/Controllers/CategoryController.cs
@@ -1,5 +1,7 @@
 ﻿using Blog.Data;
+using Blog.Extensions;
 using Blog.Models;
+using Blog.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -11,52 +13,93 @@ namespace Blog.Controllers
 		[HttpGet("v1/categories")]
 		public async Task<IActionResult> GetAsync([FromServices] BlogDataContext context)
 		{
-			var categories = await context.Categories.ToListAsync();
-			return Ok(categories);
+			try
+			{
+				var categories = await context.Categories.ToListAsync();
+				return Ok(new ResultViewModel<List<Category>>(categories));
+			}
+			catch
+			{
+				return StatusCode(500, new ResultViewModel<List<Category>>("Falha interna no servidor"));
+			}
 		}
 
 		[HttpGet("v1/categories/{id:int}")]
 		public async Task<IActionResult> GetByIdAsync(
-			[FromRoute] int id, 
+			[FromRoute] int id,
 			[FromServices] BlogDataContext context)
 		{
-			var category = await context.Categories.FirstOrDefaultAsync(s => s.Id == id);
+			try
+			{
+				var category = await context.Categories.FirstOrDefaultAsync(s => s.Id == id);
 
-			if (category == null)
-				return NotFound();
+				if (category == null)
+					return NotFound(new ResultViewModel<Category>("Não foi possível localizar a categoria"));
 
-			return Ok(category);
+				return Ok(new ResultViewModel<Category>(category));
+			}
+			catch
+			{
+				return StatusCode(500, new ResultViewModel<Category>("Erro interno no servidor"));
+			}
 		}
 
 		[HttpPost("v1/categories")]
 		public async Task<IActionResult> PostAsync(
-			[FromBody] Category category, [FromServices] BlogDataContext context
+			[FromBody] EditorCategoryViewModel model, [FromServices] BlogDataContext context
 			)
 		{
-			await context.Categories.AddAsync(category);
-			await context.SaveChangesAsync();
+			try
+			{
+				if (!ModelState.IsValid)
+					return BadRequest(new ResultViewModel<Category>(ModelState.GetErrors()));
 
-			return Ok(category);
+				var category = new Category
+				{
+					Id = 2,
+					Name = model.Name,
+					Slug = model.Slug,
+					Posts = null
+				};
+
+				await context.Categories.AddAsync(category);
+				await context.SaveChangesAsync();
+
+				return Created($"v1/categories/{category.Id}", new ResultViewModel<Category>(category));
+
+			}
+			catch (Exception ex)
+			{
+				return StatusCode(500, new ResultViewModel<Category>("Erro interno no servidor"));
+			}
 		}
 
 		[HttpPut("v1/categories/{id:int}")]
 		public async Task<IActionResult> PutAsync(
-			[FromRoute] int id, [FromBody] Category category, [FromServices] BlogDataContext context
+			[FromRoute] int id, [FromBody] EditorCategoryViewModel category, [FromServices] BlogDataContext context
 			)
 		{
-			var categoria = await context.Categories
-				.FirstOrDefaultAsync(x => x.Id == id);
+			try
+			{
+				var categoria = await context.Categories
+					.FirstOrDefaultAsync(x => x.Id == id);
 
-			if (categoria == null)
-				return NotFound();
+				if (categoria == null)
+					return NotFound(new ResultViewModel<Category>("Categoria não localizada"));
 
-			categoria.Name = category.Name;
-			categoria.Slug = category.Slug;
+				categoria.Name = category.Name;
+				categoria.Slug = category.Slug;
 
-			context.Categories.Update(categoria);
-			await context.SaveChangesAsync();
+				context.Categories.Update(categoria);
+				await context.SaveChangesAsync();
 
-			return Ok(categoria);
+				return Ok(new ResultViewModel<Category>(categoria));
+
+			}
+			catch (Exception ex)
+			{
+				return StatusCode(500, new ResultViewModel<Category>("05X01 - Não foi possível editar a categoria"));
+			}
 		}
 
 		[HttpDelete("v1/categories/{id:int}")]
@@ -64,15 +107,22 @@ namespace Blog.Controllers
 			[FromRoute] int id, [FromServices] BlogDataContext context
 			)
 		{
-			var category = await context.Categories.FirstOrDefaultAsync(c => c.Id == id);
+			try
+			{
+				var category = await context.Categories.FirstOrDefaultAsync(c => c.Id == id);
 
-			if (category == null)
-				return NotFound();
+				if (category == null)
+					return NotFound(new ResultViewModel<Category>("Categoia não localizada"));
 
-			context.Categories.Remove(category);
-			await context.SaveChangesAsync();
+				context.Categories.Remove(category);
+				await context.SaveChangesAsync();
 
-			return Ok(category);
+				return Ok(new ResultViewModel<Category>(category));
+			}
+			catch
+			{
+				return StatusCode(500, new ResultViewModel<Category>("Erro interno no servidor"));
+			}
 		}
 	}
 }
---
+++ Blog/ViewModels/EditorCategoryViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Blog.ViewModels
+{
+	public class EditorCategoryViewModel
+	{
+        [Required]
+        [StringLength(40, MinimumLength = 3, ErrorMessage = "O campo deve possuir entre 3 e 40 caracteres")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "O campo slug é obrigatorio")]
+        public string Slug { get; set; }
+    }
+}
