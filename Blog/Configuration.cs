1c3437f1e4752defd8faf29794ed6bc8b9b4acea
Eder Vieira de Oliveira Ribeiro <eder.edy@gmail.com>
2025-02-14 15:02:31 -0400

    Configuração de envio de email

--- Blog/Configuration.cs
+++ Blog/Configuration.cs
@@ -5,5 +5,14 @@
 		public static string JwtKey = "E6E17C4F4D5E437FBEA16EEC9F089A05";
 		public static string ApiKeyName = "api_key";
 		public static string ApiKey = "curso_api_123456789";
+		public static SmtpConfiguration Smtp = new();
+
+		public class SmtpConfiguration()
+		{
+            public string Host { get; set; }
+            public int Port { get; set; }
+            public string UserName { get; set; }
+            public string Password { get; set; }
+        }
     }
 }
--- Blog/Program.cs
+++ Blog/Program.cs
@@ -8,20 +8,7 @@ using System.Text;
 var builder = WebApplication.CreateBuilder(args);
 var key = Encoding.ASCII.GetBytes(Configuration.JwtKey);
 
-builder.Services.AddAuthentication(x =>
-{
-	x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-	x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-}).AddJwtBearer(x =>
-{
-	x.TokenValidationParameters = new TokenValidationParameters
-	{
-		ValidateIssuerSigningKey = true,
-		IssuerSigningKey = new SymmetricSecurityKey(key),
-		ValidateIssuer = false,
-		ValidateAudience = false
-	};
-});
+ConfigurationAuthentication(builder);
 
 builder.Services
 	.AddControllers()
@@ -29,14 +16,51 @@ builder.Services
 	{
 		options.SuppressModelStateInvalidFilter = true;
 	});
-builder.Services.AddDbContext<BlogDataContext>();
 
-builder.Services.AddTransient<TokenService>();
+ConfigurationService(builder);
 
 var app = builder.Build();
+
+ConfigurationEmail(app);
 
 app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+void ConfigurationAuthentication(WebApplicationBuilder builder)
+{
+	builder.Services.AddAuthentication(x =>
+	{
+		x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+		x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+	}).AddJwtBearer(x =>
+	{
+		x.TokenValidationParameters = new TokenValidationParameters
+		{
+			ValidateIssuerSigningKey = true,
+			IssuerSigningKey = new SymmetricSecurityKey(key),
+			ValidateIssuer = false,
+			ValidateAudience = false
+		};
+	});
+}
+
+void ConfigurationService(WebApplicationBuilder builder)
+{
+	builder.Services.AddDbContext<BlogDataContext>();
+	builder.Services.AddTransient<TokenService>();
+	builder.Services.AddTransient<EmailService>();
+}
+
+void ConfigurationEmail(WebApplication app)
+{
+	Configuration.JwtKey = app.Configuration.GetValue<string>("JwtKey");
+	Configuration.ApiKey = app.Configuration.GetValue<string>("ApiKey");
+	Configuration.ApiKeyName = app.Configuration.GetValue<string>("ApiKeyName");
+
+	var smtp = new Configuration.SmtpConfiguration();
+	app.Configuration.GetSection("Smtp").Bind(smtp);
+	Configuration.Smtp = smtp;
+}
